d4e7b620105032cf6674bafacb6353824e674efd
ShahabRasool <shahabrasool12@gmail.com>
2025-01-01 12:49:57 +0500

    inital changes

--- src/Css.php
+++ src/Css.php
@@ -3,19 +3,23 @@ class Css{
     private $shahab;
     const PADDING = "p-16";
     public static $count=0;
+    public $finalMarkup="";
    public  $tailwind=["text-red-300", "text-2xl"];
 public function __construct($shahab)
 {
    $this->shahab = $shahab;
     // print "My name is " .$shahab;
 }
-public function output(){
+public function prepareMarkup(){
     $tring= Utility::tailwindOutput($this->tailwind);
     //  print '<h1 class="' . implode(" ", $this->tailwind) . '">My name is ' . $this->shahab . '</h1>';
    
-    print '<h1 class="' . $tring . ' ' . self::PADDING . '">My name is ' . $this->shahab . '</h1>';
+    $this->finalMarkup='<h1 class="' . $tring . ' ' . self::PADDING . '">My name is ' . $this->shahab . '</h1>';
 
 
+}
+public function render(){
+    echo $this->finalMarkup;
 }
 
 }
